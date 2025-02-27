6b890cf820cf80266a7fc4cf33adfc920a9bfa35
Nirjhar <nirjharsingha@gmail.com>
2025-02-28 00:25:23 +0600

    unnecessary files deleted

--- server/src/main/java/com/example/travelApp/repositories/GroupMemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.travelApp.repositories;
-
-import com.example.travelApp.models.GroupMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GroupMemberRepository extends JpaRepository<GroupMember, Integer> {
-}
